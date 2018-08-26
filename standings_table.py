9556c5130da67c2479245475981956b47fd7cb84
asobey <alex.sobey@gmail.com>
2018-08-26 02:07:34 -0500

    AAdded Comment

--- standings_table.py
+++ standings_table.py
@@ -6,7 +6,8 @@ from tabulate import tabulate
 working = 'online'
 position = 'TE' # OPTIONS: QB, RB, RB/WR, WR, TE, FLEX, D/ST, K
 
-if working == 'online':
+if working == 'online': # setting up offline option so troublshooting can be done without
+    # navigating to the website each time.
     browser = webdriver.Firefox()
     browser.get('http://games.espn.com/ffl/standings?leagueId=413011&seasonId=2018')
 
@@ -14,7 +15,7 @@ if working == 'online':
     standings_source.write(browser.page_source)
     standings_source.close()
 
-PS = open('standings_source', 'r')
+PS = open('standings_page', 'r')
 soup = BeautifulSoup(PS,'lxml')
 table = soup.find_all('table')[0]
 df = pd.read_html(str(table))
