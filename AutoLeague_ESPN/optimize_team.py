ca071262cb58c7bfb23742f8b829c66049347fec
asobey <alex.sobey@gmail.com>
2018-08-31 17:47:59 -0500

    Got all of the login and player data working. Got the table parsing working. Currently working on ranking players by position. Then the actions of swapping them.

--- AutoLeague_ESPN/optimize_team.py
+++ AutoLeague_ESPN/optimize_team.py
@@ -2,18 +2,43 @@ import pandas as pd
 from tabulate import tabulate
 import pandas_bs4_table_parse
 
-team_table = pandas_bs4_table_parse.create_team_table('front_page_source')
-
-team_table_str = tabulate(team_table, headers='keys', tablefmt='psql')
-print(team_table_str)
-print(team_table.columns.values)
-print(team_table.index.values)
-
 positions = ['QB', 'TE', 'K', 'D/ST', 'RB', 'WR']
 
-for pos in positions:
-    #print(team_table)
-    print(pos)
-    # This could be a problem: ValueError: cannot index with vector containing NA / NaN values
-    # Try Except will not work. Maybe remove index if not filled
-    print(team_table.index[team_table[1].str.contains(pos)].values)
+def optimize(file_location, file_name):
+
+    table = pandas_bs4_table_parse.create_team_table(file_location + file_name)
+
+def rank_by_pos(table, pos_indexes):
+    for i in pos_true:
+        print(team_table['PLAYER'][i] + ': ' + player_value(team_table, i))
+    rank_db = team_table['PLAYER', 'PROJ'][pos_true]
+    print(rank_db)
+    rank_db.sort_values(by=['PROJ'])
+    print(rank_db)
+
+def player_value(table, pos_index):
+    return table[pos_index]
+
+
+if __name__ == '__main__':
+    source_file_locations = '..\\offline_webpages\\'
+    source_file_name = 'front_page_source'
+
+    team_table = pandas_bs4_table_parse.create_team_table(source_file_locations, source_file_name)
+    pandas_bs4_table_parse.print_table(team_table)
+
+    table = team_table # Used temporarily
+
+    for pos in positions:
+        print('Optimizing: ' + pos + ' .........', end=' ')
+
+        pos_true = table.index[table['POS'].str.contains(pos)].values
+        if len(pos_true) == 0:
+            raise ValueError('Position not filled!')
+        elif len(pos_true) == 1:
+            print('Only 1 ' + pos + ' on roster. No Changes made')
+        else:
+            if pos == 'RB':
+                for i in pos_true:
+                    print(team_table['PLAYER'][i] + ': ' + player_value(team_table, i))
+                rank_by_pos(team_table, pos_true)
